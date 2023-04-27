0a60fed4deb56b9bf16c00b7af182f46135041cd
WonderfulSunshine <39552770+wonderfulsunshine@users.noreply.github.com>
2023-04-27 22:25:34 +0800

    Initial commit

---
+++ RayTracingPPSN/src/WalnutApp.cpp
@@ -0,0 +1,81 @@
+#include <dinput.h>
+#include <glm/fwd.hpp>
+#include <glm/ext/scalar_uint_sized.hpp>
+
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+#include "Walnut/Random.h"
+using namespace Walnut ;
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+    virtual void OnUIRender() override
+    {
+        ImGui::Begin("Setting");
+        if (ImGui::Button("Render"))
+        {
+            Render();
+        }
+        ImGui::End();
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.f));
+
+        ImGui::Begin("ViewPort");
+        m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+        m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+
+        if (m_Image)
+            ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+        ImGui::End();
+
+        ImGui::PopStyleVar();
+    }
+
+    void Render()
+    {
+        if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+        {
+            m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+
+            delete[] m_ImageData;
+            m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+        }
+
+
+        for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+        {
+            m_ImageData[i] = Random::UInt();
+            m_ImageData[i] |= 0xff000000;
+        }
+        m_Image->SetData(m_ImageData);
+    }
+
+private:
+    std::shared_ptr<Image> m_Image;
+    uint32_t* m_ImageData = nullptr;
+    uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+    Walnut::ApplicationSpecification spec;
+    spec.Name = "Ray Tracing by pianpianshaonian";
+
+    Walnut::Application* app = new Walnut::Application(spec);
+    app->PushLayer<ExampleLayer>();
+    app->SetMenubarCallback([app]()
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Exit"))
+            {
+                app->Close();
+            }
+            ImGui::EndMenu();
+        }
+    });
+    return app;
+}
